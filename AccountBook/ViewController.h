f260fb20bdca1cda39f8b92799ba35b648b4d037
junhaoshen <junhaoshen@junhaoshendemac-mini.local>
2016-01-05 14:08:42 +0800

    Initial Commit

---
+++ AccountBook/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AccountBook
+//
+//  Created by junhaoshen on 16/1/5.
+//  Copyright © 2016年 junhaoshen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AccountBook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AccountBook
+//
+//  Created by junhaoshen on 16/1/5.
+//  Copyright © 2016年 junhaoshen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
